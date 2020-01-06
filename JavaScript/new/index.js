805f89ab5abf50327a0d1a42e319c674903a4c2f
chenwangji <694093637@qq.com>
2020-01-07 00:10:55 +0800

    feat: 实现手写 instanceof

--- JavaScript/new/index.js
+++ JavaScript/new/index.js
@@ -1,7 +1,7 @@
 /**
  * 手动实现 new 操作符
+ * @param {*} fn 构造函数，第二个及后面的参数传递给构造函数
  */
-
 function New(fn) {
     const instance = {}
     instance.__proto__ = fn.prototype
@@ -24,3 +24,28 @@ function Dog(name) {
 var dog = New(Dog, 'Tony')
 console.log(dog.name) // 'Tony'
 console.log(dog instanceof Dog) // true
+
+
+/**
+ * 
+ * @param {*} instance 实例
+ * @param {*} clazz 类
+ */
+ function instanceOf(instance, clazz) {
+    if ( typeof clazz !== 'function') throw new Error('Right-hand error')
+    if (instance === null || (typeof instance !== 'object' && typeof instance !== 'function')) return false
+
+    const proto = clazz.prototype
+    while (instance.__proto__) {
+        if (instance.__proto__ === proto) return true
+        instance = instance.__proto__
+    }
+    return false
+ }
+
+ // test
+ var one = new Number(1)
+ console.log(
+    instanceOf(one, Number), // true
+    instanceOf(one, Object) // true
+ )
